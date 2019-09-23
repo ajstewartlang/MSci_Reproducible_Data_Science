9f776dc3ba8535c4dd748a26a43de781581a68cc
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-09-23 16:43:21 +0100

    Update ggridges_example.R

--- workshop_5/r_scripts/ggridges_example.R
+++ workshop_5/r_scripts/ggridges_example.R
@@ -6,7 +6,7 @@ mpg %>%
   ggplot(aes(x = cty, y = fct_reorder(manufacturer, cty), group = manufacturer)) +
   geom_density_ridges(scale = 5, size = 0.5, rel_min_height = 0.01) +
   labs(title = "Ridge Plot of City Fuel Consumption for Different Car Manufacturers", 
-       x = "City fuel consumption (mpg)",
+       x = "City Fuel Consumption (MPG)",
        y = NULL) +
   theme_ridges() 
   
