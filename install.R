43bde2b57e5bc68e4502f000db6104e5e483fd2d
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-09-23 16:50:17 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,5 @@
-install.packages("tidyverse", "Hmisc)
+install.packages(c("tidyverse", "Hmisc", "car", "MASS", "broom", "leaps", "olsrr", 
+                   "gvlma", "ggcorrplot", "afex", "emmeans", "lme4", "ordinal", 
+                   "lmerTest", "ggforce", "concaveman", "nycflights13", "ggimage", 
+                   "gganimate", "gamlss"))
+
